a0364d04be6ae24436ddd050a783e05524594871
Tiernan OToole <tiernan@tiernanotoole.ie>
2016-12-25 13:59:39 +0000

    tweaking timing stuff

--- SnmpReader/Program.cs
+++ SnmpReader/Program.cs
@@ -35,8 +35,9 @@ namespace SnmpReader
                 { 16, "outbound" }
             };
 
+             
+            Dictionary<string, Tuple<double, DateTime>> oldCounts = new Dictionary<string, Tuple<double, DateTime>>();
 
-            Dictionary<string, double> oldCounds = new Dictionary<string, double>();
             var client = new RestClient("http://192.168.1.36:8086");
 
 
@@ -52,6 +53,7 @@ namespace SnmpReader
                     foreach (var r in counts)
                     {
                         string identifier = string.Format("1.3.6.1.2.1.2.2.1.{0}.{1}", r.Key, i.Key);
+                        string key = string.Format("{0}.{1}", r.Key, i.Key);
 
                         var data = Messenger.GetAsync(VersionCode.V1,
                                    new IPEndPoint(IPAddress.Parse(server), 161),
@@ -62,36 +64,48 @@ namespace SnmpReader
                         Console.WriteLine("Data for: {0} - {1} - {2}", i.Value, r.Value, identifier);
                         var item = data.Result.First();
 
-                        if (!oldCounds.ContainsKey(string.Format("{0}.{1}", r.Key, i.Key)))
+                        if (!oldCounts.ContainsKey(key))
                         {
-                            oldCounds[string.Format("{0}.{1}", r.Key, i.Key)] = double.Parse(item.Data.ToString());
+                            oldCounts[string.Format("{0}.{1}", r.Key, i.Key)] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
                         }
                         else
                         {
-                            double diff = double.Parse(item.Data.ToString()) - oldCounds[string.Format("{0}.{1}", r.Key, i.Key)];
-                            oldCounds[string.Format("{0}.{1}", r.Key, i.Key)] = double.Parse(item.Data.ToString());
-                            Console.WriteLine("Diff {0} bits: {1} bps", diff, diff/timespan);
+                            double diff = double.Parse(item.Data.ToString()) - oldCounts[key].Item1;
+                            
+                            var diffTime = TimeSpan.FromTicks(DateTime.Now.Ticks - oldCounts[key].Item2.Ticks).TotalSeconds;
 
-                            InfluxDb db = new InfluxDb("http://192.168.1.36:8086", "root", "root");
-                            var write = db.WriteAsync("pfsense",
-                                new Point()
-                                {
-                                    Measurement = "bandwidth", 
-                                    Fields = new Dictionary<string, object>
-                                    {
-                                       
-                                        {"bps", diff/timespan }
-                                    },
-                                    Tags = new Dictionary<string, object>()
-                                    {
-                                        { "server", "pfsense" } , { "host", "pfsense" },
-                                        {"interface", i.Value },
-                                        {"direction", r.Value },
-                                    },
-                                    Precision = InfluxDB.Net.Enums.TimeUnit.Seconds
-                                });
+                            var bps = diff / diffTime;
 
-                            Console.WriteLine(write.Result.Body);                          
+                            Console.WriteLine("Diff {0} bits: {1} bps", diff, bps);
+
+                            if (bps > 0)
+                            {
+                                oldCounts[key] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
+                                InfluxDb db = new InfluxDb("http://192.168.1.36:8086", "root", "root");
+                                var write = db.WriteAsync("pfsense",
+                                    new Point()
+                                    {
+                                        Measurement = "bandwidth",
+                                        Fields = new Dictionary<string, object>
+                                        {
+                                        {"bps", bps }
+                                        },
+                                        Tags = new Dictionary<string, object>()
+                                        {
+                                            { "server", "pfsense" } ,
+                                            { "host", "pfsense" },
+                                            {"interface", i.Value },
+                                            {"direction", r.Value },
+                                        },
+                                        Precision = InfluxDB.Net.Enums.TimeUnit.Seconds
+                                    });
+
+                                Console.WriteLine(write.Result.Body);
+                            }
+                            else
+                            {
+
+                            }                    
                         }
 
                     }
