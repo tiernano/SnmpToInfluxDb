ae4c927c3bcb922ee31c6f71a8794f842dffccb5
Tiernan OToole <tiernan@tiernanotoole.ie>
2016-12-27 18:40:56 +0000

    adding exception handling...

--- SnmpReader/Program.cs
+++ SnmpReader/Program.cs
@@ -48,63 +48,68 @@ namespace SnmpReader
 
                     foreach (var r in counts)
                     {
-                        string identifier = string.Format("1.3.6.1.2.1.2.2.1.{0}.{1}", r.Key, i.Key);
-                        string key = string.Format("{0}.{1}", r.Key, i.Key);
+                        try {
+                            string identifier = string.Format("1.3.6.1.2.1.2.2.1.{0}.{1}", r.Key, i.Key);
+                            string key = string.Format("{0}.{1}", r.Key, i.Key);
 
-                        var data = Messenger.GetAsync(VersionCode.V1,
-                                   new IPEndPoint(IPAddress.Parse(server), 161),
-                                   new OctetString("public"),
-                                   new List<Variable> { new Variable(new ObjectIdentifier(identifier)) });
+                            var data = Messenger.GetAsync(VersionCode.V1,
+                                       new IPEndPoint(IPAddress.Parse(server), 161),
+                                       new OctetString("public"),
+                                       new List<Variable> { new Variable(new ObjectIdentifier(identifier)) });
 
 
-                        Console.WriteLine("Data for: {0} - {1} - {2}", i.Value, r.Value, identifier);
-                        var item = data.Result.First();
+                            Console.WriteLine("Data for: {0} - {1} - {2}", i.Value, r.Value, identifier);
+                            var item = data.Result.First();
 
-                        if (!oldCounts.ContainsKey(key))
-                        {
-                            oldCounts[string.Format("{0}.{1}", r.Key, i.Key)] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
-                        }
-                        else
-                        {
-                            double diff = double.Parse(item.Data.ToString()) - oldCounts[key].Item1;
-                            
-                            var diffTime = TimeSpan.FromTicks(DateTime.Now.Ticks - oldCounts[key].Item2.Ticks).TotalSeconds;
-
-                            var bps = diff / diffTime;
-
-                            Console.WriteLine("Diff {0} bits: {1} bps", diff, bps);
-
-                            if (bps > 0)
+                            if (!oldCounts.ContainsKey(key))
                             {
-                                oldCounts[key] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
-                                InfluxDb db = new InfluxDb(ConfigurationManager.AppSettings["influxdbserver"], ConfigurationManager.AppSettings["influxdbusername"], ConfigurationManager.AppSettings["influxdbpassword"]);
-                                var write = db.WriteAsync(ConfigurationManager.AppSettings["influxdbname"],
-                                    new Point()
-                                    {
-                                        Measurement = "bandwidth",
-                                        Fields = new Dictionary<string, object>
-                                        {
-                                        {"bps", bps }
-                                        },
-                                        Tags = new Dictionary<string, object>()
-                                        {
-                                            { "server", ConfigurationManager.AppSettings["servername"] } ,
-                                            { "host", ConfigurationManager.AppSettings["servername"] },
-                                            { "interface", i.Value },
-                                            { "direction", r.Value },
-                                        },
-                                        Precision = InfluxDB.Net.Enums.TimeUnit.Seconds
-                                    });
-
-                                Console.WriteLine(write.Result.Body);
+                                oldCounts[string.Format("{0}.{1}", r.Key, i.Key)] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
                             }
                             else
                             {
-                                oldCounts[key] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
-                                Console.WriteLine("BPS is less than 0... skipping write...");
-                            }                    
-                        }
+                                double diff = double.Parse(item.Data.ToString()) - oldCounts[key].Item1;
 
+                                var diffTime = TimeSpan.FromTicks(DateTime.Now.Ticks - oldCounts[key].Item2.Ticks).TotalSeconds;
+
+                                var bps = diff / diffTime;
+
+                                Console.WriteLine("Diff {0} bits: {1} bps", diff, bps);
+
+                                if (bps > 0)
+                                {
+                                    oldCounts[key] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
+                                    InfluxDb db = new InfluxDb(ConfigurationManager.AppSettings["influxdbserver"], ConfigurationManager.AppSettings["influxdbusername"], ConfigurationManager.AppSettings["influxdbpassword"]);
+                                    var write = db.WriteAsync(ConfigurationManager.AppSettings["influxdbname"],
+                                        new Point()
+                                        {
+                                            Measurement = "bandwidth",
+                                            Fields = new Dictionary<string, object>
+                                            {
+                                        {"bps", bps }
+                                            },
+                                            Tags = new Dictionary<string, object>()
+                                            {
+                                                { "server", ConfigurationManager.AppSettings["servername"] } ,
+                                                { "host", ConfigurationManager.AppSettings["servername"] },
+                                                { "interface", i.Value },
+                                                { "direction", r.Value },
+                                            },
+                                            Precision = InfluxDB.Net.Enums.TimeUnit.Seconds
+                                        });
+
+                                    Console.WriteLine(write.Result.Body);
+                                }
+                                else
+                                {
+                                    oldCounts[key] = new Tuple<double, DateTime>(double.Parse(item.Data.ToString()), DateTime.Now);
+                                    Console.WriteLine("BPS is less than 0... skipping write...");
+                                }
+                            }
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine("Exception caught: {0}", ex.Message);
+                        }
                     }
                 }
                 Thread.Sleep(TimeSpan.FromSeconds(timespan));
